fb8e87f815aa40ae1f0915caa1b38c42c6e75967
jinxuefen <jinxuefen@laiyifen.com>
2020-11-07 15:23:13 +0800

    add 理解合并配置的流程

--- src/main.js
+++ src/main.js
@@ -1,35 +1,40 @@
 import Vue from 'vue'
-// import App from './App.vue'
 
-/**
- *
- * 要结合vue-2.6.12工程，一起理解，vue-2.6.12工程中有注释
- */
-
-var app = new Vue({
-  el: '#app',
-
-  render(createElement){
-    return createElement('div',{
-      // todo 如果这里写成app1会直接替换掉模板页面的index.html中 <div id="app"></div> ==>渲染成<div id="app1"></div>
-      // todo 使用render函数就不会出现从插值表达式{{message}} => hello vue显示的一个过渡，会直接显示出来；之前的过渡是因为编译器需要将template编译成render函数需要时间
-      attrs:{
-        id:'app1'
-      }
-    },this.message)
+let childComp = {
+  template:'<div>{{msg}}</div>',
+  created() {
+    console.log('child created')
   },
-  // todo 上面的也可以简写成 下面的，es6尖头函数
-  // 这种简写方式不能访问到this.message
-  // render: h => h('div',{attrs:{id:'app1'}},'测试'),
   mounted(){
-    // todo 为什么 这里的this.message可以直接访问到 data中的message
-    // new Vue(..)--->调用Vue.prototype._init()中 ---> initState() ————》
-    // 通过proxy代理，Object.definedPorperty，访问this.message实际上访问的是this._data.message
-    console.log(this.message)
+    console.log('child mounted')
   },
   data(){
     return {
-      message: 'Hello Vue'
+      msg: 'Hello Vue'
     }
   }
+}
+
+Vue.mixin({
+  created() {
+    console.log('parent created')
+  }
 })
+
+
+let app = new Vue({
+  el: '#app',
+  render: h => h(childComp)
+})
+
+/**
+ * 输出结果
+ *  parent created
+ *  parent created
+    child created
+    child mounted
+
+ debugger断点 先走Vue.mixin ===》 new Vue ==》_init(options) => mergeOptions(...)  ==》输出parent created --》 childComp==》 输出parent created child created child mounted
+ *
+ *
+ * */
